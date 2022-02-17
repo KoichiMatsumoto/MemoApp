b9d49d4c411d235a5f1a7388d533a8a9a4911200
KoichiM <mail.koichimatsumoto@gmail.com>
2022-02-17 15:37:46 +0900

    MemoEditScreen and MemoCreateScreen Created

--- App.jsx
+++ App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import MemoDetailScreen from "./src/screens/MemoDetailScreen";
 import MemoListScreen from "./src/screens/MemoListScreen";
+import MemoEditScreen from "./src/screens/MemoEditScreen";
+import MemoCreateScreen from "./src/screens/MemoCreateScreen";
 
 export default function App() {
-  return <MemoDetailScreen />;
+  return <MemoCreateScreen />;
 }
